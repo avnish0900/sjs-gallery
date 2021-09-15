ba14f5f5a93def6c229c56230abc3e1fe7c43143
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-09-15 18:17:48 +0530

    Create menuapi.js

---
+++ menuapi.js
@@ -0,0 +1,296 @@
+const Menu = [
+  {
+    id: 1,
+    image: "https://i.pinimg.com/564x/06/1c/b9/061cb9ccd74e9ee0a0891ce9eb67b764.jpg",
+    name:"Formal-Suit",
+    category: "Formal-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 33,
+    image: "./img/4883888b-a1c2-4a1e-877b-a7c5f5549652.jfif",
+    name:"Formal-Suit",
+    category: "Formal-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 34,
+    image: "https://i.pinimg.com/564x/53/94/4a/53944ab4c9ffbc606cd18d3fb6452c8d.jpg",
+    name:"Double Breast-Suit",
+    category: "Double-Breast Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 35,
+    image: "https://i.pinimg.com/564x/77/5b/49/775b49033856fae31c5d362f20bc3604.jpg",
+    name:"Double Breast-Suit",
+    category: "Double-Breast Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 2,
+    image: "./img/59e88c73-478a-4ba1-8cac-2fec6d44e5d0.jfif",
+    name:"Party-Wear",
+    category: "Party-Wear-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 3,
+    image: "./img/8f96241b-2243-4a80-8886-ec0046925d55.jfif",
+    name:"Business-Suit",
+    category: "Casual-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 4,
+    image:"https://i.pinimg.com/564x/91/bb/d5/91bbd5ef4c3aae64cd86e8c308456f79.jpg",
+    name: "Party Wear",
+    category: "Party Wear",
+    bigCategory:"Suit",
+    price: "12₹",
+    
+  },
+
+  {
+    id: 5,
+    image: "https://i.pinimg.com/564x/93/6e/c0/936ec0a55b3dae1ddfc36f57bbc21b99.jpg",
+    name:"Casual-Suit",
+    category: "Casual-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 6,
+    image: "https://i.pinimg.com/564x/3f/b7/38/3fb738b74036c19a032cb9f99bf37117.jpg",
+    name:"Business-Suit",
+    category: "Business-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 7,
+    image: "https://i.pinimg.com/564x/d9/63/1d/d9631d1f4c0ff618df1bdbb709990d13.jpg",
+    name:"Formal-Pent",
+    category: "Formal-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  {
+    id: 8,
+    image: "https://i.pinimg.com/564x/d5/5c/28/d55c28a3ab82825d8e4896ba790b946f.jpg",
+    name:"Formal-Pent",
+    category: "Formal-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  {
+    id: 9,
+    image: "https://i.pinimg.com/564x/78/d5/42/78d542e99edfde90fc893372a7fef387.jpg",
+    name:"Casual-Pent",
+    category: "Casual-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  {
+    id: 10,
+    image: "https://i.pinimg.com/564x/17/ad/0b/17ad0bfd6a8934bc6390bad389426241.jpg",
+    name:"Suit's-Pent",
+    category: "Suit's-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  {
+    id: 36,
+    image: "https://i.pinimg.com/564x/f1/71/6f/f1716f84990444c8247a4f5db6ad71a8.jpg",
+    name:"Suit's-Pent",
+    category: "Suit's-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  
+  {
+    id: 11,
+    image: "https://i.pinimg.com/564x/c8/4c/98/c84c98d535f767648a06acae598f9e0a.jpg",
+    name:"Suit's-Pent",
+    category: "Suit's-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  {
+    id: 12,
+    image: "./img/1.jfif",
+    name:"Casual-Shirt",
+    category: "Casual-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 13,
+    image: "./img/2.jfif",
+    name:"Casual-Shirt",
+    category: "Casual-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 14,
+    image: "https://i.pinimg.com/564x/1f/e5/e8/1fe5e8d7006e0a99ea6d82d78bbb3d91.jpg",
+    name:"Pintex-Shirt",
+    category: "Pintex-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 15,
+    image: "./img/3.jfif",
+    name:"Formal-Shirt",
+    category: "Formal-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 16,
+    image: "./img/7.jfif",
+    name:"Chinese-Collar-Shirt",
+    category: "Chinese-Collar-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 17,
+    image: "https://i.pinimg.com/564x/61/14/e6/6114e6c7f2093128f4fcb8efd8c4c29d.jpg",
+    name:"Casual-Shirt",
+    category: "Casual-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 18,
+    image: "./img/5.jfif",
+    name:"Formal-Shirt",
+    category: "Formal-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 19,
+    image: "./img/6.jfif",
+    name:"Casual-Shirt",
+    category: "Casual-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 20,
+    image: "https://i.pinimg.com/564x/f2/aa/b0/f2aab0e3c929fa036e1ec5236d016fd2.jpg",
+    name:"Party-Wear-Shirt",
+    category: "Party-Wear-Shirt",
+    bigCategory:"Shirt",
+    price: "20₹"
+  },
+  {
+    id: 21,
+    image: "./img/55.jfif",
+    name:"Party-Wear",
+    category: "Party-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 22,
+    image: "https://i.pinimg.com/564x/68/f2/7f/68f27f8006fa356374f1e0f81aa0b0c3.jpg",
+    name:"Weeding-Wear",
+    category: "Weeding-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 23,
+    image: "https://i.pinimg.com/564x/bc/74/ba/bc74bae856366417ca0ea39e848ffa1a.jpg",
+    name:"Party-Wear",
+    category: "Party-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 24,
+    image: "https://i.pinimg.com/564x/85/1d/32/851d32ab2fdf082e96f505fb52ab6603.jpg",
+    name:"Weeding-Wear",
+    category: "Weeding-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 25,
+    image: "https://i.pinimg.com/564x/3f/0a/85/3f0a85b477eef7b0848f9f49d3818574.jpg",
+   name:"Weeding-Wear",
+    category: "Weeding-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 26,
+    image: "https://i.pinimg.com/564x/34/f5/46/34f546e0f420b1353523959eb5ae09d8.jpg",
+    name:"Weeding-Wear",
+    category: "Weeding-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 27,
+    image: "https://i.pinimg.com/564x/99/de/51/99de519dd5174f9b6ffb7afad1cca069.jpg",
+   name:"Casual-Wear",
+    category: "Casual-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+  {
+    id: 28,
+    image: "./img/11.jfif",
+    name:"Formal-Suit",
+    category: "Formal-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 29,
+    image: "./img/33.jfif",
+    name:"Formal-Suit",
+    category: "Formal-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 30,
+    image: "./img/44.jfif",
+    name:"Formal-Suit",
+    category: "Formal-Suit",
+    bigCategory:"Suit",
+    price: "20₹"
+  },
+  {
+    id: 31,
+    image: "./img/66.jfif",
+    name:"Formal-Pent",
+    category: "Formal-Pent",
+    bigCategory:"Trouser",
+    price: "20₹"
+  },
+  {
+    id: 32,
+    image: "https://i.pinimg.com/564x/44/7c/af/447caf117f445f9e5081b0c4fe9c3541.jpg",
+    name:"Weeding-Wear",
+    category: "Weeding-Wear",
+    bigCategory:"Sherwani",
+    price: "20₹"
+  },
+
+];
+
+export default Menu;
